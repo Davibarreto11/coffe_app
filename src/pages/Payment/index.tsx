0790bd80a17d068e586d00302620dace2a8aba89
Davi Barreto <daviarturdesa686@gmail.com>
2024-02-25 22:34:31 -0300

    Component Payment

---
+++ src/pages/Payment/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { StyleSheet, Text } from 'react-native'
+
+const Payment: React.FC = () => {
+  return <Text>Payment</Text>
+}
+
+const styles = StyleSheet.create({});
+
+export default Payment;
