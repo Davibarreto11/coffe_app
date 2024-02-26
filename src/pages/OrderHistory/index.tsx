0d8a46e1d92dadfd73623dd57330268c97082450
Davi Barreto <daviarturdesa686@gmail.com>
2024-02-25 22:34:19 -0300

    Component OrdeHistory

---
+++ src/pages/OrderHistory/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyleSheet, Text } from "react-native";
+
+const OrderHistory: React.FC = () => {
+  return <Text>Order History</Text>
+}
+
+const styles = StyleSheet.create({});
+
+export default OrderHistory;
