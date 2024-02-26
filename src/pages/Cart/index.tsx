6d728ef731902731563a25d540c8da160725613e
Davi Barreto <daviarturdesa686@gmail.com>
2024-02-25 22:33:52 -0300

    Component Cart

---
+++ src/pages/Cart/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyleSheet, Text } from "react-native";
+
+const Cart: React.FC = ()  => {
+  return <Text>Cart</Text>
+}
+
+const styles = StyleSheet.create({});
+
+export default Cart;
