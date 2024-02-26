bf32d6993ef1f523de6d70f0743e646e9ed2f380
Davi Barreto <daviarturdesa686@gmail.com>
2024-02-25 22:34:05 -0300

    Component Favorite

---
+++ src/pages/Favorite/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyleSheet, Text } from "react-native";
+
+const Favorite: React.FC = () => {
+  return <Text>Favorite</Text>
+}
+
+const styles = StyleSheet.create({});
+
+export default Favorite;
