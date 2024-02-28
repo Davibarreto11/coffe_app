cacf92974886e1c65e2f4da5f1d5ae5aedf1064c
Davi Barreto <daviarturdesa686@gmail.com>
2024-02-28 19:58:18 -0300

    List Length items cart

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { StyleSheet, Text } from "react-native";
+import { useStore } from "../../store/store";
 
 const Cart: React.FC = ()  => {
+  const CartList = useStore((state: any) => state.CartList);
+  console.log('Cartlist =', CartList.length);
+
   return <Text>Cart</Text>
 }
 
